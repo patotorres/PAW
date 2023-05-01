f8974463f3a8fe8105d36708ec7bcd62a64a82c0
fedesin <fedesimone31@gmail.com>
2023-04-30 23:13:07 -0300

    se agrega script bootstrap.php

--- public/index.php
+++ public/index.php
@@ -1,19 +1,10 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../src/bootstrap.php';
 
 use Paw\App\Controllers\PageController;
 use Paw\App\Controllers\ErrorController;
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
 
-$log = new Logger('mvc-app');
-$log->pushHandler(new StreamHandler(__DIR__.'/../logs/app.log',Logger::DEBUG));
-
-
-$whoops = new \Whoops\Run;
-$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
-$whoops->register();
 
 $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 $log->info("Petición a: {$path}");
