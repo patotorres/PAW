f12d2aa325744267e95bd2dc72856a8863a55224
Pato Torres <petorres92@gmail.com>
2023-05-04 14:28:02 -0300

    Actualizo script para iniciar servidor

--- scripts/00-run-composer.sh
+++ scripts/00-run-composer.sh
@@ -2,5 +2,8 @@
 echo "Running composer"
 composer install --no-dev --working-dir=/var/www/html
 
-service nginx stop
+echo "Sopping nginx"
+systemctl stop nginx
+
+echo "Starting php built-in server"
 php -S 0.0.0.0:80 -t /var/www/html/public/
