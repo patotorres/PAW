afd09762fe79106e914988216e22035ae3b4e34f
Pato Torres <petorres92@gmail.com>
2023-05-05 15:05:57 -0300

    Mejoro footer

--- src/App/Views/parts/footer.view.php
+++ src/App/Views/parts/footer.view.php
@@ -1,24 +1,32 @@
 <footer>
-    <li class="certificadoFooter">
-    <p>Avalado por</p>
-    <a class="logoCertificadoFooter" href="https://www.argentina.gob.ar/salud"target=_blank></a>    
-    </li>
-    <li class="contactoFooter">
-    <p>Contacto</p>
-    <address>
-        <ul>
-        <li><a class="mail" href="mailto:juanperez@email.com">Correo electrónico: juanperez@email.com</a></li>
-        <li>Teléfono: +1 (555) 555-5555</li>
-        <li>Dirección: 123 Calle Principal, Luján, 6700</li>
-        </ul>  
-    </address>
-    </li>
-    <li class="redesFooter">
-    <p>Redes</p>
     <ul>
-        <a href="https://instagram.com"target=_blank><img src="/assets/img/instagram.png" width="48" height="48" /></a>
-        <a href="https://twitter.com"target=_blank><img src="/assets/img/twitter.png" width="48" height="48" /></a>
-        <a href="https://facebook.com"target=_blank><img src="/assets/img/facebook.png" width="48" height="48" /></a>
+        <li class="certificadoFooter">
+            <h4>Avalado por</h4>
+            <a class="logoCertificadoFooter" href="https://www.argentina.gob.ar/salud"target=_blank></a>
+        </li>
+        <li class="contactoFooter">
+            <h4>Contacto</h4>
+            <address>
+                <ul>
+                    <li><a class="mail" href="mailto:juanperez@email.com">Correo electrónico: juanperez@email.com</a></li>
+                    <li>Teléfono: +1 (555) 555-5555</li>
+                    <li>Dirección: 123 Calle Principal, Luján, 6700</li>
+                </ul>
+            </address>
+        </li>
+        <li class="redesFooter">
+            <h4>Redes</h4>
+            <ul>
+                <li>
+                    <a href="https://instagram.com"target=_blank><img src="/assets/img/instagram.png" width="48" height="48" /></a>
+                </li>
+                <li>
+                    <a href="https://twitter.com"target=_blank><img src="/assets/img/twitter.png" width="48" height="48" /></a>
+                </li>
+                <li>
+                    <a href="https://facebook.com"target=_blank><img src="/assets/img/facebook.png" width="48" height="48" /></a>
+                </li>
+            </ul>
+        </li>
     </ul>
-    </li>
 </footer>
