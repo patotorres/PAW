dbf8331e78503cfaabb5878d200b15b4d64f3581
Federico Simone <fedesimone31@gmail.com>
2023-05-16 16:26:09 -0300

    linkeo el head con js pero falta

--- src/App/Views/parts/head.view.php
+++ src/App/Views/parts/head.view.php
@@ -3,3 +3,6 @@
 <title>UNLu PAW UL Hospitals</title>
 <link rel="stylesheet" type="text/css" href="/assets/css/main.css">
 <link href="https://fonts.cdnfonts.com/css/gotham" rel="stylesheet">
+
+<script src="/assets/js/app.js"></script>
+<script src="/assets/js/components/PAW.js"></script>
