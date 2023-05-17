06445085840df46af537540bc665a0d075ac2110
SalvadorScafati <seviperclash@gmail.com>
2023-05-17 19:40:45 -0300

    ultimos retoques drag and drop

--- public/assets/js/components/PAWdragdropfile.js
+++ public/assets/js/components/PAWdragdropfile.js
@@ -3,16 +3,36 @@ class PAWdragdropfile {
 constructor(){
     const dropzone = document.getElementById('dropzone');
     const preview = document.getElementById('preview');
-    
+
+dropzone.addEventListener('change', function() {
+    const file = this.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => {
+          const img = document.createElement("img");
+          img.src = reader.result;
+          preview.innerHTML = "";
+          preview.appendChild(img);
+        };
+    }
+});
 // Manejadores de eventos para prevenir comportamientos predeterminados
 dropzone.addEventListener('dragenter', (e) => {
   e.preventDefault();
   e.stopPropagation();
+  dropzone.classList.add('dragover');
 });
+
+dropzone.addEventListener("dragleave", () => {
+    dropzone.classList.remove("dragover");
+});
+  
 
 dropzone.addEventListener('dragover', (e) => {
   e.preventDefault();
   e.stopPropagation();
+  dropzone.classList.add("dragover");
 });
 
 dropzone.addEventListener('drop', (e) => {
@@ -38,6 +58,7 @@ dropzone.addEventListener('drop', (e) => {
   } else {
     alert('Archivo no válido');
   }
+  dropzone.classList.remove("dragover");
 });
 
 // Función para validar el archivo
